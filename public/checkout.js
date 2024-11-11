ed42ff7f47212ce4638f92891c7ec89e91c3a9bb
gerymen <mohammedsiraj2013@gmail.com>
2024-11-11 14:10:27 +0530

    change payment method to  emmeded code

--- public/checkout.js
+++ public/checkout.js
@@ -1,99 +1,39 @@
 // This is your test publishable API key.
-const stripe = Stripe("pk_test_51QAVMQDMFjia7oQIlHEniCh963qx2t1kf3z9v8HB2CBza8tHXOySwrJYwCiBF3WcGpmcFnYQbnLrlYXaqrr3anEl008UAvcufQ");
+const stripe = Stripe("pk_live_51PuZw1FeZszO3OaWHqs54NUtXMSomib5Xyr8XNqKRluWLQ5NpTe4M9rDZsrsbrzmhL35KcL330MllqX6e6lCuylJ00DWA0b81z");
 
 // The items the customer wants to buy
 const items = [{ id: "xl-tshirt", amount: 1000 }];
 
 let elements;
 
+
 initialize();
 
-document
-  .querySelector("#payment-form")
-  .addEventListener("submit", handleSubmit);
-
-// Fetches a payment intent and captures the client secret
+// Create a Checkout Session
 async function initialize() {
-  const response = await fetch("https://test-node-js-pearl.vercel.app/api/payments/create-payment-intent", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      "amount": 90000,
-      "country": "CN"
-    }),
-  });
-  const { clientSecret, dpmCheckerLink } = await response.json();
-
-  const appearance = {
-    theme: 'stripe',
-  };
-  elements = stripe.elements({ appearance, clientSecret });
-  
-  const paymentElementOptions = {
-    paymentMethodOrder: ['apple_pay', 'google_pay', 'card', 'klarna']
+  const fetchClientSecret = async () => {
+    const response = await  fetch("http://localhost:3000/api/payments/create-payment-intent", {
+      method: "POST",
+    })
+        
+    const { clientSecret } = await response.json();
+    console.log('clientSecret', clientSecret);
+    return clientSecret;
   };
 
-  const paymentElement = elements.create("payment", paymentElementOptions);
-  paymentElement.mount("#payment-element");
-
-  // [DEV] For demo purposes only
-  setDpmCheckerLink(dpmCheckerLink);
-}
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  setLoading(true);
-
-  const { error } = await stripe.confirmPayment({
-    elements,
-    confirmParams: {
-      // Make sure to change this to your payment completion page
-      return_url: "http://localhost:4242/complete.html",
-    },
+  const checkout = await stripe.initEmbeddedCheckout({
+    fetchClientSecret
   });
 
-  // This point will only be reached if there is an immediate error when
-  // confirming the payment. Otherwise, your customer will be redirected to
-  // your `return_url`. For some payment methods like iDEAL, your customer will
-  // be redirected to an intermediate site first to authorize the payment, then
-  // redirected to the `return_url`.
-  if (error.type === "card_error" || error.type === "validation_error") {
-    showMessage(error.message);
-  } else {
-    showMessage("An unexpected error occurred.");
-  }
-
-  setLoading(false);
+  // Mount Checkout
+  await checkout.mount('#checkout');
 }
-
-// ------- UI helpers -------
-
-function showMessage(messageText) {
-  const messageContainer = document.querySelector("#payment-message");
-
-  messageContainer.classList.remove("hidden");
-  messageContainer.textContent = messageText;
-
-  setTimeout(function () {
-    messageContainer.classList.add("hidden");
-    messageContainer.textContent = "";
-  }, 4000);
-}
-
-// Show a spinner on payment submission
-function setLoading(isLoading) {
-  if (isLoading) {
-    // Disable the button and show a spinner
-    document.querySelector("#submit").disabled = true;
-    document.querySelector("#spinner").classList.remove("hidden");
-    document.querySelector("#button-text").classList.add("hidden");
-  } else {
-    document.querySelector("#submit").disabled = false;
-    document.querySelector("#spinner").classList.add("hidden");
-    document.querySelector("#button-text").classList.remove("hidden");
-  }
-}
-
-function setDpmCheckerLink(url) {
-  document.querySelector("#dpm-integration-checker").href = url;
-}
+// const response = await fetch("http://localhost:3000/api/payments/create-payment-intent", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({
+//     "amount": 90000,
+//     "country": "CN"
+//   }),
+// });
+// const { clientSecret, dpmCheckerLink } = await response.json();
